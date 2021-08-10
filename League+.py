97f8ec5ea64ba77816d301e4d1f3d48352f0ab72
Sebastian Avila <sebavi782@gmail.com>
2021-08-10 16:04:10 -0700

    First commit

---
+++ League+.py
@@ -0,0 +1,71 @@
+from os import name
+import os
+import spotipy
+import spotipy.util as util
+import time
+from json.decoder import JSONDecodeError
+from secondary import songs
+from secondary import playlists
+from secrets import secrets
+
+notes_path = "C:\Riot Games\League of Legends\MyNotes.txt"
+
+def authenticate():
+    try:
+        token = util.prompt_for_user_token(
+            username=secrets['username'],
+            scope='user-read-private user-read-playback-state user-modify-playback-state user-read-recently-played',
+            client_id=secrets['client_id'],
+            client_secret=secrets['client_secret'],
+            redirect_uri='https://www.google.com/'
+        )
+    except (AttributeError, JSONDecodeError):
+        os.remove(f".cache-{secrets['username']}")
+        token = util.prompt_for_user_token(
+            username=secrets['username'],
+            scope='user-read-private user-read-playback-state user-modify-playback-state user-read-recently-played',
+            client_id=secrets['client_id'],
+            client_secret=secrets['client_secret'],
+            redirect_uri='https://www.google.com/'
+        )
+    return token
+
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+def play(song_name, spotify_object, deviceID):
+    try:
+        spotify_object.add_to_queue(songs[song_name], deviceID)
+        spotify_object.next_track(deviceID)
+    except KeyError:
+        pass
+
+def playlist(playlist_name, spotify_object, deviceID):
+    try:
+        playlist_name = playlists[playlist_name]
+        for track in spotify_object.playlist_items(playlist_name)['items']:
+            spotify_object.add_to_queue(track['track']['uri'], deviceID)
+    except KeyError:
+        pass
+
+commands_to_functions = {"/play" : play, '/playlist' : playlist}
+
+
+if __name__ == "__main__":
+    spotify_object = spotipy.Spotify(auth=authenticate())
+    devices = spotify_object.devices()
+    deviceID = devices['devices'][0]['id']
+    logfile = open(notes_path,"r")
+    loglines = follow(logfile)
+    for line in loglines:
+        command = line.split()
+        if not command:
+            continue
+        if command[0] in commands_to_functions:
+            commands_to_functions[command[0]](' '.join(command[1:]), spotify_object, deviceID)
---
+++ secondary.py
@@ -0,0 +1,29 @@
+playlists = {
+    'spanish' : 'https://open.spotify.com/playlist/4ksBgJpi57gRMBVEOL8uDz?si=4272f1a41b0541a7',
+    'anime' : 'https://open.spotify.com/playlist/0FPBnNGVLmTZ8etTZ1ZARF?si=92b13c905db847e4',
+    'jojo' : 'https://open.spotify.com/playlist/67qtHfrlfS5jqjfRA9ugds?si=53be066dae024f5b',
+    '2020' : 'https://open.spotify.com/playlist/37i9dQZF1EMe8tYHxB4vS5?si=8623f1a179764b69',
+    'trap' : 'https://open.spotify.com/playlist/3qQTcZGLJL9MkEnlQOdE1p?si=49af1b353a574bcd',
+    'lol' : 'https://open.spotify.com/playlist/2DzGFDnTMgp0fDil2v438w?si=260be144c3714de2',
+    'kpop' : 'https://open.spotify.com/playlist/5y9BH0Xfskxmg1IRDq1lsX?si=524274c8ccee4de8'
+}
+
+
+songs = {'yonaguni': 'spotify:track:2JPLbjOn0wPCngEot2STUS', 'dakiti': 'https://open.spotify.com/track/47EiUVwUp4C9fGccaPuUCS',
+ 'la barca': 'spotify:track:1J5700UDj49giM0Cciu7By', 'corazon corazon': 'spotify:track:0r3UK1RqFIDtl7RYs6X1ty', 'el caballo de mi padre': 'spotify:track:7geALLlb83RRB8FX4BEBIw', 
+ 'por tu maldito amor': 'spotify:track:3KZI2xeiIRJd8fm84ClxH5', 'que de raro tiene': 'spotify:track:3gA2qpAbnuKLQV8Q9ZtIcf', 'la diferencia': 'spotify:track:6wwMqdntt3An3mmVSb613i',
+  'volver volver': 'spotify:track:4Fvnz1ZJ86IdqDAepWYPAh', 'estos celos': 'spotify:track:6u6kH7V7Tx5xDJtF82vVw0', 'un millon de primaveras': 'spotify:track:6j5LtifAnuTjTYvml61yFZ',
+   'aca entre nos': 'spotify:track:2FDFz5TuYlpP180AUicAcl', 'todo de ti': 'spotify:track:4fSIb4hdOQ151TILNsSEaF', 'dile que tu me quieres': 'spotify:track:5u5MvmVtitax9R1Mxh3reC',
+    'lo que paso, paso': 'spotify:track:6z5Xrx58p5Bs18RIe6KXbI', 'la cancion': 'spotify:track:0fea68AdmYNygeTGI4RC18',
+     'sanguinarios del m1': 'spotify:track:4EAhDuoqyRtV4WiuN49OUe', 'nieves de enero': 'spotify:track:3fNK9wZMHV7CLG1z3rueBO',
+      'el gato de chihuahua': 'spotify:track:1PKoT3dROitNnZQch89TgT', 'a mis enemigos': 'spotify:track:0rRxqhX6w0IWTFSzzRFSOo',
+       'alma enamorada': 'spotify:track:0gySORG1cXDarlA9LEQvSi', 'lamberto quintero': 'spotify:track:71uSlH35CcZhYR8IjPfBzD', 'de museo': 'spotify:track:267NGliXM8YLVZiKAD9Otm',
+        'corazon sin cara': 'spotify:track:0u4N6w5lBw5xMrzJitRWUo', 'como se siente - remix': 'spotify:track:2tFwfmceQa1Y6nRPhYbEtC',
+         'botella tras botella': 'spotify:track:2MnLkFqY9Rpg9s3Zv6ZJdK', 'watermelon sugar': 'spotify:track:6UelLqGlWMcVH1E5c4H7lY', 
+'all around me': 'spotify:track:5Py8zRKGkZvgHniVVtvNCN', 'celebrate': 'spotify:track:0b6UoVjUzFyZNpHI0aAhKt',
+ 'up all night': 'spotify:track:1mpD5Q8IM32I4bF6eCpU74', "don't start now": 'spotify:track:3PfIrDoz19wz7qK7tYeu62', 'be kind': 'spotify:track:3Z8FwOEN59mRMxDCtb8N0A',
+  'lola': 'spotify:track:6hkr5ZGVOt3Z2BNGE6yqpB', 'get me (feat. kehlani)': 'spotify:track:5VwwpU7KsLhgfbMEecedhJ', 'hold on': 'spotify:track:1nahzW3kfMuwReTka28tH5',
+   'leave the door open': 'spotify:track:7MAibcTli4IisCtbHKrGMh', 'eleven': 'spotify:track:1ToprX3cpBiXoAe5eNSk74', 'why': 'spotify:track:7lozLnUfzcgEpMMVQI7yhA',
+    'sunflower': 'spotify:track:0RiRZpuVRbi7oqRdSMwhQY', 'come around me': 'spotify:track:2o9LAypwGH4ctV0i9boo6d',
+     'happier': 'spotify:track:2dpaYNEQHiRxtZbfNsse99', "that's what i like": 'spotify:track:0KKkJNfGyhkQ5aFogxQAPU', 'better': 'spotify:track:6zeeWid2sgw4lap2jV61PZ',
+      'treat you better': 'spotify:track:3QGsuHI8jO1Rx4JWLUh9jd', 'what do you mean?': 'spotify:track:4B0JvthVoAAuygILe3n4Bs'}
